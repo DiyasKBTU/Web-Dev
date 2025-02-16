380bc6f6c53e5a4bf1ebab3467e12cfc3b857380
DiyasKBTU <157691950+DiyasKBTU@users.noreply.github.com>
2025-02-16 17:22:24 +0500

    Add files via upload

---
+++ Lab4/src/app/product-item/product-item.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import { Product } from '../amazon';
+
+@Component({
+  selector: 'app-product-item',
+  templateUrl: './product-item.component.html',
+  styleUrls: ['./product-item.component.css']
+})
+export class ProductItemComponent implements OnInit {
+
+  @Input() product!: Product;
+  @Output() redirect = new EventEmitter();
+  @Output() open = new EventEmitter();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
